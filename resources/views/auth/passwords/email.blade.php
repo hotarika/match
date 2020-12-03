aedb3fcb6428197d8797bb4b8530391cc09ddcb6
tatata <mugicha8932@gmail.com>
2020-12-03 10:14:24 +0900

    Coding / パスワードリセット / classの適用

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
+<main class="l-main p-reset">
+   <div class="container">
+      <div class="l-main__mainArea -oneColumn_auth">
+         <section class="c-h2__sec">
+            <div class="row justify-content-center">
+               <h2 class="c-h2__head">パスワードをリセット</h2>
+               <div class="card-body">
+                  @if (session('status'))
+                  <div class="alert alert-success" role="alert">
+                     {{ session('status') }}
+                  </div>
+                  @endif
+                  <div class="c-h2__oneRowBody p-reset__body">
+                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
+                        <div class="p-reset__inputWrap">
+                           <input
+                              id="email"
+                              type="email"
+                              class="c-form__input @error('email') is-invalid @enderror"
+                              name="email"
+                              value="{{ old('email') }}"
+                              required
+                              autocomplete="email"
+                              autofocus
+                              placeholder="メールアドレス" />
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                           @error('email')
+                           <span class="c-form__invalid" role="alert">
+                              <strong>{{$message}}</strong>
+                           </span>
+                           @enderror
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        <button type="submit" class="c-btn p-reset__submitBtn">リセット</button>
+                     </form>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
-</div>
+         </section>
+      </div>
+   </div>
+</main>
 @endsection
