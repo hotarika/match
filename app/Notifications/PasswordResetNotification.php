14f8449ddfeb0148be06c3509cc7d6237b0075f8
tatata <mugicha8932@gmail.com>
2020-12-03 12:26:47 +0900

    Laravel / パスワード再設定メールの日本語化

---
+++ app/Notifications/PasswordResetNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Lang;
+
+class PasswordResetNotification extends ResetPassword
+{
+    use Queueable;
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        if (static::$toMailCallback) {
+            return call_user_func(static::$toMailCallback, $notifiable, $this->token);
+        }
+
+        return (new MailMessage)
+            ->subject(Lang::get('パスワード再設定'))
+            ->line(Lang::get('下のボタンをクリックしてパスワードを再設定してください。'))
+            ->action(Lang::get('パスワード再設定'), url(route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
+            ->line(Lang::get('パスワード再設定の有効期限は :count 分です。', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
+            ->line(Lang::get('もし心当たりがない場合は、このメッセージを破棄してください。'));
+    }
+}
