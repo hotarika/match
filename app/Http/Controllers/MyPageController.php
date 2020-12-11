05746b2492f699b36a15d648a4c29dcd840f9f35
tatata <mugicha8932@gmail.com>
2020-12-12 02:42:45 +0900

    デプロイ修正

---
+++ app/Http/Controllers/MyPageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class MyPageController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        // abort(500);
+
+        // *******************************
+        // 通知機能
+        // *******************************
+        $user = User::find(Auth::id());
+        foreach ($user->unreadNotifications as $key => $value) {
+            if ($value) {
+                $notification[$key] = $value;
+            }
+        }
+        // 通知がなかった場合はundefinedになるため、null値を格納してエラー回避
+        if (!isset($notification)) {
+            $notification = null;
+        }
+
+        // マイページの通知以外のデータは非同期（axios）により取得
+        return view('mypage', compact('notification'));
+    }
+}
