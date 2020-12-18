b6dafff9dae72495e82b5c7ec18640bd1f655daa
tatata <mugicha8932@gmail.com>
2020-12-19 08:14:39 +0900

    TDKの設定

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,9 +2,11 @@
    <meta charset="UTF-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-   <meta name="description" content="ページの内容を表す文章" />
    <meta name="csrf-token" content="{{ csrf_token() }}">
    <title>@yield('title') | match</title>
+   <meta name="description"
+      content="気軽に簡単にITの受注・発注ができるサービスmatch。 WEB制作、デザイン、SNSマーケティングなどあなたのスキルを生かしてみませんか？ また、もしITに関するお仕事を頼みたい場合もmatchで解決することができます。 さあ、あなたも始めてみましょう！" />
+   <meta name="keywords" content="IT,仕事,受注,発注,気軽に,簡単に,スキル," />
    <link
       rel="stylesheet"
       href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
